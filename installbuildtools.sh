93cb78554ea4fb5bcf7a4eeeb83c2741f872e03c
James Coliz <jcoliz@xbox.com>
2019-10-05 21:52:47 -0700

    Create installbuildtools.sh

---
+++ installbuildtools.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib \
+     build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
+     xz-utils debianutils iputils-ping libsdl1.2-dev xterm \
+     git cmake curl libcurl4-openssl-dev libssl-dev uuid-dev jq
